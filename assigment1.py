352321db7ac1e5b1aabba09b93c069fde93a1393
saklanisahil <saklanisahil2@gmail.com>
2026-01-05 17:47:51 +0530

    commit message

---
+++ assigment1.py
@@ -0,0 +1,12 @@
+#input two number
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+#arithmetic operations
+addition=num1+num2
+print("Addition:", addition)
+subtraction=num1-num2
+print("Subtraction:", subtraction)
+multiplication=num1*num2
+print("Multiplication:", multiplication)
+division=num1/num2
+print("Division:", division)
---
+++ assigment2.py
@@ -0,0 +1,6 @@
+#take user first name and last name as input 
+first_name=input("enter the first name:")
+last_name=input("enter the last name:")
+#concatenate first name and last name
+name=first_name+last_name
+print("HELLO ,", name+"!"+" Welcome to python programming.")
