5ef677b0bc53649d92ab22fc96c3df4244b69756
saklanisahil <saklanisahil2@gmail.com>
2026-01-14 11:58:52 +0530

    Add files via upload

---
+++ task2/1.py
@@ -0,0 +1,3 @@
+#odd even using if else
+num=int(input("enter the number:"))
+print ("even") if num%2==0 else print("odd")
---
+++ task2/2.py
@@ -0,0 +1,7 @@
+#sum of n number using loop
+total=0
+n=int(input("enter the number you want the total to be calculate upto:"))
+for i in range(1,n+1):
+    total+=i
+print("the sum of first n number is ",total)
+    
