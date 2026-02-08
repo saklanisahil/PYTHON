4f3b327e0752cc5b1c670a16e49915596afd31e4
saklanisahil <saklanisahil2@gmail.com>
2026-02-08 18:28:05 +0530

    Delete maths.py

--- maths.py
+++
@@ -1,8 +0,0 @@
-import math
-num=float(input("enter the number "))
-
-
-
-print(f"square root :{math.sqrt(num)} ")
-print(f"natural log :{math.log(num)}")
-print(f" sine :{math.sin(math.radians(num))}")
