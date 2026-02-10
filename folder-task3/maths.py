b1a8f60f2d03f7c250fa7a84b1a8c557ff6a229b
saklanisahil <saklanisahil2@gmail.com>
2026-02-10 19:46:46 +0530

    Refactor math operations into separate functions
    Added functions for square root, natural log, and sine calculations.

--- folder-task3/maths.py
+++ folder-task3/maths.py
@@ -1,8 +1,22 @@
 import math
 num=float(input("enter the number "))
-
-
-
-print(f"square root :{math.sqrt(num)} ")
-print(f"natural log :{math.log(num)}")
-print(f" sine :{math.sin(math.radians(num))}")
+def sqr(num):
+    if num<0:
+        print("the function for this number dont exits")
+    else:
+        print(f"square root :{math.sqrt(num)} ")
+def log(num):
+    if num<=0:
+        print("log doesnt exits ")
+    else :
+        print(f"natural log :{math.log(num)}")        
+    
+def sine(num):    
+    print(f" sine :{math.sin(math.radians(num))}")
+    
+    
+    
+sqr(num)
+log(num)
+sine(num)
+    
